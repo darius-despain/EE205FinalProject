26046a74626c814367e11d66601a4158298148d2
TButuyan <tylerbutuyan@gmail.com>
2017-04-16 19:56:24 -0700

    updated tamagotchi class and added food.cpp

---
+++ food.cpp
@@ -0,0 +1,62 @@
+// food related functions for tamagotchi class
+// Tyler Butuyan
+// tbutuyan
+// C Monsters
+// 4/16/2017
+
+void tamagotchi::hunger() {
+    --hunger;
+    return;
+}
+
+bool tamagotchi::feed(std::string food) {
+    // tamagotchi refuse to eat
+    if (hunger >= 4) {
+        return false;
+    }
+    // tamagotchi eats a meal
+    if (food == "meal") {
+        hunger += 2;
+        if (hunger > 4) {
+            weight += (hunger - 4);
+        }
+        return true;
+    }
+    // tamagotchi eats a snack
+    if (food == "snack") {
+        ++hunger;
+        if (hunger > 4) {
+            weight += (hunger - 4);
+        }
+        return true;
+    }
+    return false;
+}
+
+void tamagotchi::discipline() {
+
+}
+
+int tamagotchi::form() {
+    if (age < 1) {          // egg form
+        form = 0;
+    }
+    if (age < 5) {          // baby form
+        form = 1;
+    }
+    else if (age < 10) {    // child form
+        form = 2;
+    }
+    else if (age > 17) {    // adult form
+        if (hunger == 4 && happiness == 4) {    // excellent form
+            form = 3;
+        }
+        if (hunger >= 2 && happiness >= 2) {    // average form
+            form = 4;
+        }
+        if (hunger < 2 && happiness < 2) {      // bad form
+            form = 5;
+        }
+    }
+    return form;
+}
--- tamagotchi.h
+++ tamagotchi.h
@@ -1,9 +1,10 @@
-
 // Header file for tamagotchi class
 // Tyler Butuyan
 // tbutuyan
 // C Monsters
 // 4/16/2017
+
+#include <string>
 
 class tamagotchi{
 private:
@@ -13,20 +14,22 @@ private:
     int age;            // 0 - MAXAGE
     int weight;         // 0 - MAXWEIGHT
     int discipline;     // 0 - MAXDISC
-    int sick;           // 0 or 1
-    int sleep;          // 0 or 1
+    bool sick;          // true or false
+    bool sleep;         // true or false
+    bool attention;     // true or false
     int hygiene;        // 0 - MAXHYG
     int form;           // 0 - MAXFORM
 public:
-    void hunger();       // decrease hunger value by 1
-    void sick();         // set sick variable to 1
-    void feed();         // increase hunger value, keep track of overfeeding
-    void light();        // set to 1 if tamagotchi awake, if 1 and sleeping decrease happiness
-    void poop();         // decrease hygiene value, lower than threshold decrease health by 1
-    void hygiene();      // increase hygiene value
-    void play();         // increase happiness by 1 if won, decrease if lost
-    void medicine();     // set sick variable to 0
-    void attention();    // send notification to user
-    void discipline();   // increase discipline variable 
-    void sleep();        // set sleep variable to 1
+    void hunger();      // decrease hunger value by 1
+    void sick();        // set sick variable to 1
+    bool feed();        // increase hunger value, keep track of overfeeding
+    void light();       // set to 1 if tamagotchi awake, if 1 and sleeping decrease happiness
+    void poop();        // decrease hygiene value, lower than threshold decrease health by 1
+    void hygiene();     // increase hygiene value
+    void play();        // increase happiness by 1 if won, decrease if lost
+    void medicine();    // set sick variable to 0
+    bool attention();   // send notification to user, return true if notification sent
+    void discipline();  // increase discipline variable 
+    void sleep();       // set sleep variable to 1
+    int form();         // set the form of tamagotchi
 };
