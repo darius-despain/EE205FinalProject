a06ea873f0e81ffb0756503867db10e3cddbfb93
Darius Despain <Darius Despain>
2017-04-20 15:17:05 -1000

    added descriptions etc

--- tamagotchi.h
+++ tamagotchi.h
@@ -6,11 +6,11 @@
 
 #include <string>
 
-#define MAXHEALTH
-#define MAXAGE
-#define MAXWEIGHT
-#define MAXHDISC
-#define MAXHYG
+#define MAXHEALTH 10
+#define MAXAGE 15
+#define MAXWEIGHT 20
+#define MAXDISC 15
+#define MAXHYG 10
 
 class tamagotchi{
 private:
@@ -25,17 +25,28 @@ private:
     bool attention;     // true or false
     int hygiene;        // 0 - MAXHYG
     int form;           // 0 - 5
+    bool light;         // true or false; current light setting
 public:
     void hunger();      // decrease hunger value by 1
     void sick();        // set sick variable to 1
+    bool attention();   // set attention variable to 1, return true if notification sent
+    void sleep();       // set sleep variable to 1
+
     bool feed();        // increase hunger value, keep track of overfeeding
-    void light();       // set to 1 if tamagotchi awake, if 1 and sleeping decrease happiness
+    void lightSwitch();  // set to 1 if tamagotchi awake, if 1 and sleeping decrease happiness
     void poop();        // check if needs to poop, decrease hygiene value, lower than threshold decrease health by 1
     void clean();     // increase hygiene value by cleaning up
     void play();        // increase happiness by 1 if won, decrease if lost
     void medicine();    // set sick variable to 0
-    bool attention();   // set attention variable to 1, return true if notification sent
     void discipline();  // increase discipline variable
-    void sleep();       // set sleep variable to 1
     int form();         // set the form of tamagotchi
+
+    void statDisplay(); //displays statistics to user
+    void mainDisplay(); //displays the "Game Window" including all elements. Considers light and other background conditions
+    void formDisplay(); //displays the tomagotchi at current form, also displays if it is sleeping and emotions
+    void poopDisplay(); //displays poop on the screen at empty spot
+    
+
+
+
 };
