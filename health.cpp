f6d17a59328e75859422a2b45dceeb6fe265e12e
Tyler Butuyan <Tyler Butuyan>
2017-04-28 14:46:00 -1000

    updated functions for multiple functions

--- health.cpp
+++ health.cpp
@@ -1,13 +1,36 @@
+// function definitions for health related functions
+// C Monsters
+// 4/20/2017 darius, created health.cpp
+// 4/26/2017  tyler, updated functions for multiple classes
+
 #include "tamagotchi.h"
 
-// has a 10% chance of getting sick
-void tamagotchi::sickly(){
-  int n = rand() % 10;
-  if(n == 0){
+void egg::sickly() { return; }
+
+void baby::sickly() {         // 20% chance
+  if((rand() % 20) == 0) {
     sick = true;
   }
-
 }
+
+void teenager::sickly() {     // 10% chance
+  if((rand() % 10) == 0) {
+    sick = true;
+  }
+}
+
+void adult::sickly() {        // 15% chance
+  if((rand() % 15) == 0) {
+    sick = true;
+  }
+}
+
+void senior::sickly() {       // 20% chance
+  if((rand() % 20) == 0) {
+    sick = true;
+  }
+}
+
 
 // has a 40% chance of getting healthy
 void tamagotchi::medicine(){
@@ -17,25 +40,55 @@ void tamagotchi::medicine(){
   }
 }
 
+void egg::poop() { return; }
+
+void baby::poop() {
+  if((rand() % 20) == 0){       // 20% chance of poop
+    hygiene--;
+  }
+  // have a chance of reducing happiness proportional to hygiene
+  if(n <= hygiene && hygiene != MAXHYG){
+    if(happiness != 0) happiness -= 1;
+  }
+  if( hygiene == MAXHYG && n <= 5 && happiness != 4) happiness += 1;
+}
+
+void teenager::poop() {
+  if((rand() % 20) == 0){       // 20% chance of poop
+    hygiene--;
+  }
+  // have a chance of reducing happiness proportional to hygiene
+  if(n <= hygiene && hygiene != MAXHYG){
+    if(happiness != 0) happiness -= 1;
+  }
+  if( hygiene == MAXHYG && n <= 5 && happiness != 4) happiness += 1;
+}
+
+void adult::poop() {
+  if((rand() % 10) == 0){       // 10% chance of poop
+    hygiene--;
+  }
+  // have a chance of reducing happiness proportional to hygiene
+  if(n <= hygiene && hygiene != MAXHYG){
+    if(happiness != 0) happiness -= 1;
+  }
+  if( hygiene == MAXHYG && n <= 5 && happiness != 4) happiness += 1;
+}
+
+void senior::poop() {
+  if((rand() % 20) == 0){       // 20% chance of poop
+    hygiene--;
+  }
+  // have a chance of reducing happiness proportional to hygiene
+  if(n <= hygiene && hygiene != MAXHYG){
+    if(happiness != 0) happiness -= 1;
+  }
+  if( hygiene == MAXHYG && n <= 5 && happiness != 4) happiness += 1;
+}
+
 // increase hygiene value by cleaning up
 void tamagotchi::clean(){
   if(hygiene != MAXHYG ) happiness += 2;
   if(happiness >= 4) happiness = 4;
   hygiene = MAXHYG;
-}
-
-// check if needs to poop, decrease hygiene value, lower than threshold decrease health by 1
-void tamagotchi::poop(){
-  // check if it needs to poop
-  int n = rand() % 10;
-  if(n == 0){
-    // if it does, increase number of poop on screen
-    hygiene--;
-  }
-  // have a chance of reducing happiness proportional to hygiene
-
-  if(n <= hygiene && hygiene != MAXHYG){
-    if(happiness != 0) happiness -= 1;
-  }
-  if( hygiene == MAXHYG && n <= 5 && happiness != 4) happiness += 1;
 }
